99d55defc7ceda4b9ea179f95f0d03cfff436513
semihseven <semihseven0@gmail.com>
2025-10-26 16:20:18 +0300

    challange 1 and 3 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,70 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        System.out.println(Main.checkForPalindrome("I did, did I?"));
+//        System.out.println(Main.checkForPalindrome("hello"));
+//        System.out.println(Main.checkForPalindrome("Was it a car or a cat I saw ?"));
+
+        System.out.println(Main.convertDecimalToBinary(5));
+        System.out.println("*******************");
+        System.out.println(Main.convertDecimalToBinary(6));
+        System.out.println("*******************");
+        System.out.println(Main.convertDecimalToBinary(13));
+        System.out.println("*******************");
+
     }
+
+    public static boolean checkForPalindrome (String sentence) {
+        String trimmedSentence = sentence.replaceAll("[\\p{Punct}\\s]+","").toLowerCase();
+        String englishSentence = trimmedSentence.replaceAll("ı","i");
+
+        List<Character> characterList = new ArrayList<>();
+        for(char chr:englishSentence.toCharArray()) {
+            characterList.add(chr);
+        }
+        System.out.println(characterList);
+        for(int i=0;i<characterList.size();i++) {
+            if(characterList.get(i) != characterList.get(characterList.size()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String convertDecimalToBinary(int num) {
+        String result="";
+
+        ArrayList<Integer> numberList = new ArrayList<>();
+
+        while(num>0) {
+            numberList.add(num%2);
+            num /= 2;
+            System.out.println(numberList);
+        }
+        Collections.reverse(numberList);
+        System.out.println(numberList);
+
+        for(int numChar : numberList) {
+            result += Integer.toString(numChar);
+        }
+
+        System.out.println(result);
+
+
+        return result;
+    }
+
+//    public String toString(ArrayList<Integer> numbers){
+//        String word;
+//
+//        for(int)
+//    }
 
 }
---
+++ src/main/java/org/example/WorkintechList.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class WorkintechList {
+}
+
+/*
+Veri Yapıları Challenge II
+org.example paketi altında WorkintechList adında bir sınıf yazmanız istenmektedir.
+
+Sınıf ArrayList sınıfının tüm özelliklerine sahip olmalı ve bir ArrayList gibi davranmalı.
+
+Ancak WorkintechList'ten oluşturulmuş instance değerleri içerilerinde bir ArrayList'ten farklı olarak
+tekrar eden data tutamazlar.
+
+WorkintechList sınıfınde sort isminde bir metot tanımlamalısınız ve WorkintechList'e eklenmiş tüm değerleri
+sıralamalı(String ise A'dan Z'ye)(numeric ise 0'dan sonsuza doğru)
+
+WorkintechList sınıfında bir remove metodu olmalı. Parametre olarak Object tipinde bir parametre almalı.
+
+remove metodu aldığı parametreyi WorkintechList'ten silmeli ve WorkintechList'te bulunan tüm elemanları
+sıralamalı.
+ */
